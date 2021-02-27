1298ea98029ab72ac005ff765af97fe6431b3e93
sergiuszorgo <67199252+sergiuszorgo@users.noreply.github.com>
2021-02-27 19:55:47 +0200

    Finish styles

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -2,6 +2,10 @@ import React from "react";
 import s from "./Statistics.module.css";
 import PropTypes from "prop-types";
 
+function cellColor() {
+  return `#${(Math.random().toString(16) + "00000").slice(2, 8)}`;
+}
+
 const Statistics = ({ title, stats }) => {
   // console.log(title, stats);
   return (
@@ -12,9 +16,13 @@ const Statistics = ({ title, stats }) => {
           {stats.map((el) => {
             const { id, label, percentage } = el;
             return (
-              <li className={s.item} key={id}>
+              <li
+                className={s.item}
+                key={id}
+                style={{ background: cellColor() }}
+              >
                 <span className={s.label}>{label}</span>
-                <span className={s.percentage}>{percentage}</span>
+                <span className={s.percentage}>{percentage}%</span>
               </li>
             );
           })}
